6aaef3c5609b6ac2feee7c8b9eea26411a92a252
BraveKoen <koen@bravemail.nl>
2021-01-11 16:53:42 +0100

    added playerMovement

--- Player.cpp
+++ Player.cpp
@@ -9,3 +9,25 @@ Player::Player(GameDataRef data):
 void Player::draw(){
     data->window.draw(playerSprite);
 }
+
+void PLayer::playerMove(bool up, bool down, bool right, bool left){
+    if(up && down){
+
+    }
+    if(right && left){
+
+    }
+    if(up){
+        playerPosition.y += 1;
+    }
+    if(down){
+        playerPosition.y -= 1;
+    }
+    if(right){
+        playerPosition.x += 1;
+    }
+    if(left){
+        playerPosition.x -= 1;
+    }
+
+}
--- Player.hpp
+++ Player.hpp
@@ -8,12 +8,11 @@ public:
     ~Player();
 
     void draw();
+    void playerMove(bool up, bool down, bool right, bool left);
 
 private:
 
     GameDataRef data;
     sf::Sprite playerSprite;
     sf::Vector2f playerPosition;
-    
-
 };
