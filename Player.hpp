ede14658ab54b7360a3b8a3099d8acfca7f1c94f
BraveKoen <koen@bravemail.nl>
2021-01-11 23:35:07 +0100

    added playermovement with taskinput

--- Player.hpp
+++ Player.hpp
@@ -1,6 +1,7 @@
 #pragma once
 
 #include <SFML/Graphics.hpp>
+#include <iostream>
 
 class Player{
 public:
@@ -8,11 +9,12 @@ public:
     ~Player();
 
     void draw();
-    void playerMove(bool up, bool down, bool right, bool left);
+    void playerMove(sf::Keyboard::Key key);
 
 private:
 
     GameDataRef data;
     sf::Sprite playerSprite;
     sf::Vector2f playerPosition;
+    uint16_t movementSpeed = 2;
 };
