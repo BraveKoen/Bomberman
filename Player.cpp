2ff19eded533c1a483ba7e77cf9f23a9b531d5c5
BraveKoen <koen@bravemail.nl>
2021-01-12 12:29:01 +0100

    Parent class added and opponent

--- Player.cpp
+++ Player.cpp
@@ -1,16 +1,43 @@
 #include "Player.hpp"
 
 Player::Player(GameDataRef data):
+    Character(data),
     data(data)
     {
         playerSprite.setTexture(data->getTexture("player.png"));
     }
 
-void Player::draw(){
+void Player::draw() {
     data->window.draw(playerSprite);
 }
 
-void Player::playerMove(sf::Keyboard::Key key){
+
+void Player::setHealth(uint8_t health){
+    playerHealth = health;
+}
+
+int Player::getHealth(){
+    return playerHealth;
+}
+
+void Player::setPosition(sf::Vector2f pos){
+    playerSprite.setPosition(pos);
+}
+
+sf::Vector2f Player::getPosition(){
+    return playerPosition;
+}
+
+void Player::setMovementSpeed(uint8_t speed) {
+    movementSpeed = speed;   
+}
+
+int Player::getMovementSpeed(){
+    return movementSpeed;
+}
+
+
+void Player::playerMove(sf::Keyboard::Key key) {
     if(sf::Keyboard::Key::Up == key){
         birdPosition.y -= movementSpeed;
     }else if (sf::Keyboard::Key::Down == key){
@@ -23,3 +50,4 @@ void Player::playerMove(sf::Keyboard::Key key){
     playerSprite.setPosition(birdPosition);
 	}
 }
+
