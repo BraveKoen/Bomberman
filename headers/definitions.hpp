7e6ae14f3ad156a550994f71adf43a8a46b0be35
Rick Horeman <rickhoreman@gmail.com>
2021-01-13 11:18:05 +0100

    Fixed some stuff based on tests, added definitions.hpp

--- headers/definitions.hpp
+++ headers/definitions.hpp
@@ -4,18 +4,17 @@
 #define screenWidth 1280
 #define screenHeight 720
 
-#define mapSelectorStateBackgroundFilepath "resources/mapSelectorStateMenuBackground.png"
-#define mapSelectorStateOptionFilepath "resources/mapSelectorStateMenuButton.png"
-
 #define solid "resources/solid.png"
 #define breakable "resources/break.png"
 #define play1 "resources/play1.png"
 #define play2 "resources/play2.png"
 #define mapBackground "resources/mapBackground.png"
 
-
 #define globalFont "resources/256BYTES.TTF"
 
-#define mapFolderLocation "resources\\maps"
+#define mapFolderLocation "resources/maps"
+
+#define mapSelectorBackgroundFilepath "../resources/test.png"
+#define mapSelectorOptionFilepath "../resources/test.png"
 
 #endif // __DEFINITIONS_HPP__ 
