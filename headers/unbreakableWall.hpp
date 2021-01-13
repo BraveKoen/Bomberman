4685bd75b2ee59caaaa3ced72acd81cd8721a14c
JorisHeemskerk <60688818+JorisHeemskerk@users.noreply.github.com>
2021-01-13 11:03:54 +0100

    changed GameData to GameDataRef

--- headers/unbreakableWall.hpp
+++ headers/unbreakableWall.hpp
@@ -8,7 +8,7 @@
 class unbreakableWall
 {
 public:
-    unbreakableWall(GameData data);
+    unbreakableWall(gameDataRef data);
 
     void spawnWall();
     void drawWall();
@@ -19,7 +19,7 @@ public:
     void setWallScale(sf::Vector2f scale);
 
 private:
-    GameData gameData;
+    gameDataRef gameData;
     std::vector<sf::Sprite> unbreakableWallSprites;
 };
 
