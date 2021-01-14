09601643d1806a04624423f7b2baab6426d66aaa
JorisHeemskerk <60688818+JorisHeemskerk@users.noreply.github.com>
2021-01-14 16:15:15 +0100

    added temp map immages and fixed errors regarding my forgetfullness

--- headers/definitions.hpp
+++ headers/definitions.hpp
@@ -7,6 +7,13 @@
 #define mapSelectorStateBackgroundFilepath "resources/mapSelectorStateMenuBackground.png"
 #define mapSelectorStateOptionFilepath "resources/mapSelectorStateMenuButton.png"
 
+#define solid "resources/solid.png"
+#define breakable "resources/break.png"
+#define play1 "resources/play1.png"
+#define play2 "resources/play2.png"
+#define mapBackground "resources/mapBackground.png"
+
+
 #define globalFont "resources/256BYTES.TTF"
 
 #define mapFolderLocation "resources\\maps"
