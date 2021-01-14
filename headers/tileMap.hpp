67243d149c71f1c6856b5f4074840e3772db64e6
Rick Horeman <rickhoreman@gmail.com>
2021-01-14 16:57:32 +0100

    lots of functions implementations

--- headers/tileMap.hpp
+++ headers/tileMap.hpp
@@ -3,16 +3,22 @@
 
 #include <SFML/Graphics.hpp>
 #include <vector>
+#include <string>
+#include "game.hpp"
 
 class TileMap{
     private:
         sf::Vector2f position;
         sf::Vector2f size;
+        gameDataRef gameData;
         sf::Vector2u mapSize;
         std::vector<std::vector<std::string>> map;
     public:
-        TileMap(sf::Vector2f position, sf::Vector2f size, sf::Vector2u mapSize=sf::Vector2u(15,15));
-        TileMap(sf::Vector2f position, sf::Vector2f size, std::vector<std::vector<std::string>> map, sf::Vector2u mapSize=sf::Vector2u(15,15));
+        TileMap(sf::Vector2f position, sf::Vector2f size, gameDataRef gameData, sf::Vector2u mapSize=sf::Vector2u(15,15));
+        TileMap(sf::Vector2f position, sf::Vector2f size, gameDataRef gameData, std::vector<std::vector<std::string>> map, sf::Vector2u mapSize=sf::Vector2u(15,15));
+        //Screen coords vs tile coords math functions
+        sf::Vector2u screenPosToTilePos(sf::Vector2f screenPosition); //Returns the tile that the given screenspace position is in
+        sf::Vector2f tilePosToScreenPos(sf::Vector2u tilePosition); //Returns the center of the tile in screenspace
         //General setters
         void setTileMapPosition(sf::Vector2f newPosition);
         void setTileMapSize(sf::Vector2f newSize);
@@ -27,9 +33,9 @@ class TileMap{
         void setTile(sf::Vector2u tilePosition, std::string type); //Based on tile coordinates
         void setTile(sf::Vector2f screenPosition, std::string type); //Based on screen coordinates
         std::string getTile(sf::Vector2u tilePosition)const; //Based on tile coordinates
-        std::string getTile(sf::Vector2f screenPosition)const; //Based on screen coordinates
+        std::string getTile(sf::Vector2f screenPosition); //Based on screen coordinates
         
-        void draw(sf::RenderWindow window);
+        void draw(bool drawPlayerSpawns=false); //Draw all terrain in tilemap
 };
 
 #endif // __TILEMAP_HPP__
--- sources/tileMap.cpp
+++ sources/tileMap.cpp
@@ -1,10 +1,27 @@
 #include "../headers/tileMap.hpp"
 
-TileMap::TileMap(sf::Vector2f position, sf::Vector2f size, sf::Vector2u mapSize):
+TileMap::TileMap(sf::Vector2f position, sf::Vector2f size, gameDataRef gameData, sf::Vector2u mapSize):
     position(position),
     size(size),
+    gameData(gameData),
     mapSize(mapSize)
     {}
+
+TileMap::TileMap(sf::Vector2f position, sf::Vector2f size, gameDataRef gameData, std::vector<std::vector<std::string>> map, sf::Vector2u mapSize):
+    position(position),
+    size(size),
+    gameData(gameData),
+    map(map),
+    mapSize(mapSize)
+    {}
+
+sf::Vector2u TileMap::screenPosToTilePos(sf::Vector2f screenPosition){  //Returns the tile that the given screenspace position is in
+    return sf::Vector2u((screenPosition.x-position.x)/(size.x/mapSize.x), (screenPosition.y-position.y)/(size.y/mapSize.y));
+}
+
+sf::Vector2f TileMap::tilePosToScreenPos(sf::Vector2u tilePosition){ //Returns the center of the tile in screenspace
+    return sf::Vector2f((size.x/mapSize.x*tilePosition.x)+position.x-(size.x/mapSize.x/2), (size.y/mapSize.y*tilePosition.y)+position.y-(size.y/mapSize.y/2));
+}
 
 void TileMap::setTileMapPosition(sf::Vector2f newPosition){
     position = newPosition;
@@ -35,21 +52,58 @@ sf::Vector2u TileMap::getMapSize()const{
 }
 
 std::vector<std::vector<std::string>> TileMap::getMap()const{
-
+    return map;
 }
 
 void TileMap::setTile(sf::Vector2u tilePosition, std::string type){
-
+    map[tilePosition.x][tilePosition.y] = type;
 }
 
 void TileMap::setTile(sf::Vector2f screenPosition, std::string type){
-
+    setTile(screenPosToTilePos(screenPosition), type);
 }
 
 std::string TileMap::getTile(sf::Vector2u tilePosition)const{
-
+    return map.at(tilePosition.x).at(tilePosition.y);
 }
 
-std::string TileMap::getTile(sf::Vector2f screenPosition)const{
+std::string TileMap::getTile(sf::Vector2f screenPosition){
+    return getTile(screenPosToTilePos(screenPosition));
+}
 
+void TileMap::draw(bool drawPlayerSpawns){
+    sf::Sprite background(gameData->assetManager.getTexture("map background"));
+    background.setPosition(position);
+    background.setScale(size.x/background.getTexture()->getSize().x, size.y/background.getTexture()->getSize().y);
+    gameData->window.draw(background);
+
+    for(unsigned int i=0; i<map.size(); i++){
+        for(unsigned int j=0; j<map[i].size(); j++){
+            if(map[i][j] != "empty"){
+                sf::Sprite sprite;
+                if(map[i][j] == "solid"){
+                    sprite.setTexture(gameData->assetManager.getTexture("unbreakable wall"));
+                }else if(map[i][j] == "break"){
+                    sprite.setTexture(gameData->assetManager.getTexture("breakable wall"));
+                }else if(drawPlayerSpawns){
+                    if(map[i][j] == "play1"){
+                        sprite.setTexture(gameData->assetManager.getTexture("player1 spawn location"));
+                    }else if(map[i][j] == "play2"){
+                        sprite.setTexture(gameData->assetManager.getTexture("player2 spawn location"));
+                    }else if(map[i][j] == "play3"){
+                        sprite.setTexture(gameData->assetManager.getTexture("player3 spawn location"));
+                    }else if(map[i][j] == "play4"){
+                        sprite.setTexture(gameData->assetManager.getTexture("player4 spawn location"));
+                    }else{
+                        break;
+                    }
+                }else{
+                    break;
+                }
+                sprite.setPosition(position.x+((size.x/mapSize.x)*i), position.y+((size.y/mapSize.y)*i));
+                sprite.setScale((size.x/mapSize.x)/sprite.getTexture()->getSize().x, (size.y/mapSize.y)/sprite.getTexture()->getSize().y);
+                gameData->window.draw(sprite);
+            }
+        }
+    }
 }
