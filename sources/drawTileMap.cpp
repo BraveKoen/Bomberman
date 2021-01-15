4556c43d90fcbbcdd447ff45a521430c139be26f
JorisHeemskerk <60688818+JorisHeemskerk@users.noreply.github.com>
2021-01-15 13:54:11 +0100

    changed .cpp to .hpp in previous fix

--- sources/drawTileMap.cpp
+++ sources/drawTileMap.cpp
@@ -1,4 +1,4 @@
-#include "../headers/drawTileMap.cpp"
+#include "../headers/drawTileMap.hpp"
 
 void drawTileMap(TileMap tileMap, gameDataRef gameData, bool drawPlayerSpawns){
     sf::Sprite background(gameData->assetManager.getTexture("map background"));
