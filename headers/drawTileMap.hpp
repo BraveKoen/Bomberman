3fbcbb64167d51863de5fe30fcb40576e28d7470
Rick Horeman <rickhoreman@gmail.com>
2021-01-15 11:31:05 +0100

    Fixed some last stuff, ready for merge

--- headers/drawTileMap.hpp
+++ headers/drawTileMap.hpp
@@ -4,41 +4,6 @@
 #include "tileMap.hpp"
 #include "game.hpp"
 
-void drawTileMap(TileMap tileMap, gameDataRef gameData, bool drawPlayerSpawns){
-    sf::Sprite background(gameData->assetManager.getTexture("map background"));
-    background.setPosition(tileMap.getTileMapPosition());
-    background.setScale(tileMap.getTileMapSize().x/background.getTexture()->getSize().x, tileMap.getTileMapSize().y/background.getTexture()->getSize().y);
-    gameData->window.draw(background);
-    std::vector<std::vector<std::string>> map = tileMap.getMap();
-    for(unsigned int i=0; i<map.size(); i++){
-        for(unsigned int j=0; j<map[i].size(); j++){
-            if(map[i][j] != "empty"){
-                sf::Sprite sprite;
-                if(map[i][j] == "solid"){
-                    sprite.setTexture(gameData->assetManager.getTexture("unbreakable wall"));
-                }else if(map[i][j] == "break"){
-                    sprite.setTexture(gameData->assetManager.getTexture("breakable wall"));
-                }else if(drawPlayerSpawns){
-                    if(map[i][j] == "play1"){
-                        sprite.setTexture(gameData->assetManager.getTexture("player1 spawn location"));
-                    }else if(map[i][j] == "play2"){
-                        sprite.setTexture(gameData->assetManager.getTexture("player2 spawn location"));
-                    }else if(map[i][j] == "play3"){
-                        sprite.setTexture(gameData->assetManager.getTexture("player3 spawn location"));
-                    }else if(map[i][j] == "play4"){
-                        sprite.setTexture(gameData->assetManager.getTexture("player4 spawn location"));
-                    }else{
-                        break;
-                    }
-                }else{
-                    break;
-                }
-                sprite.setPosition(tileMap.getTileMapPosition().x+((tileMap.getTileMapSize().x/tileMap.getMapSize().x)*i), tileMap.getTileMapPosition().y+((tileMap.getTileMapSize().y/tileMap.getMapSize().y)*j));
-                sprite.setScale((tileMap.getTileMapSize().x/tileMap.getMapSize().x)/sprite.getTexture()->getSize().x, (tileMap.getTileMapSize().y/tileMap.getMapSize().y)/sprite.getTexture()->getSize().y);
-                gameData->window.draw(sprite);
-            }
-        }
-    }
-}
+void drawTileMap(TileMap tileMap, gameDataRef gameData, bool drawPlayerSpawns);
 
 #endif // __DRAWTILEMAP_HPP__
