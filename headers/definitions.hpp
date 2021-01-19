18f7801ef483cda847238e71247b2618f6ac178d
JorisHeemskerk <60688818+JorisHeemskerk@users.noreply.github.com>
2021-01-19 14:01:14 +0100

    added animation for the player using the AnimatedSprite library

--- headers/definitions.hpp
+++ headers/definitions.hpp
@@ -30,15 +30,6 @@ namespace Resource {
     constexpr auto defaultButton = "resources/defaultButton.png";
 
     constexpr auto playerSprite = "resources/playerSprite.png";
-    constexpr auto bombFuseSprite1 = "resources/bombFuse/bombFuseSprite1.png";
-    constexpr auto bombFuseSprite2 = "resources/bombFuse/bombFuseSprite2.png";
-    constexpr auto bombFuseSprite3 = "resources/bombFuse/bombFuseSprite3.png";
-    constexpr auto bombFuseSprite4 = "resources/bombFuse/bombFuseSprite4.png";
-    
-    constexpr auto bombExplosionSprite1 = "resources/bombExplosion/bombExplosionSprite1.png";
-    constexpr auto bombExplosionSprite2 = "resources/bombExplosion/bombExplosionSprite2.png";
-    constexpr auto bombExplosionSprite3 = "resources/bombExplosion/bombExplosionSprite3.png";
-    constexpr auto bombExplosionSprite4 = "resources/bombExplosion/bombExplosionSprite4.png";
 }
 
 #endif // __DEFINITIONS_HPP__
