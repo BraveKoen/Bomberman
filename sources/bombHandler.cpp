bfa1a4cb15f18396e34cdfb2bffc69ea1faa535f
JorisHeemskerk <60688818+JorisHeemskerk@users.noreply.github.com>
2021-01-20 15:39:12 +0100

    added explosion animation

--- sources/bombHandler.cpp
+++ sources/bombHandler.cpp
@@ -22,10 +22,13 @@ void BombHandler::update(){
         if(bomb->isExploded(clock.getElapsedTime().asSeconds())){
             bombs.erase(bombs.begin() + count);
             break;
+        }else if(bomb->getPrimed()){
+            bomb->animateExplosion();
+            count++;
         }else{
             bomb->animateFuse();
             count++;
-        } 
+        }
     }
 }
 
