dc188b03a001ecdc826d23da2fc28485735c6dce
BraveKoen <koen@bravemail.nl>
2021-01-20 12:01:55 +0100

    server

--- sources/inGameState.cpp
+++ sources/inGameState.cpp
@@ -15,14 +15,18 @@ void InGameState::init(){
 
     gameData->tileMap.setTileMapPosition(sf::Vector2f(0, 0));
     gameData->tileMap.setTileMapSize(sf::Vector2f(Resource::screenHeight, Resource::screenHeight));
+<<<<<<< 10fa9c9a59fed2b237e9d585c0b62c0cbdde06f1
 
     //needs to be fixed! 
     bool useArrowKeys = 0;
+=======
+>>>>>>> server
 
     auto posTileMap = gameData->tileMap.searchForType("play1");
     auto spawnLocPlayer = sf::Vector2f{0, 0};
     auto spawnLocOpponent = sf::Vector2f{200, 0};
     if(posTileMap.size() > 0){
+<<<<<<< 94e0fd9b996c98dbe4f8af7079f0facf6407dd16
 
         spawnLocPlayer = gameData->tileMap.tilePosToScreenPos(posTileMap[0]);
         spawnLocOpponent = gameData->tileMap.tilePosToScreenPos(posTileMap[1]);
@@ -32,6 +36,12 @@ void InGameState::init(){
     players.push_back(std::make_unique<Player>(gameData, bHandler, false, spawnLocPlayer));
     opponents.push_back(std::make_unique<Opponent>(gameData, bHandler, spawnLocOpponent));
 
+=======
+        spawnLoc = gameData->tileMap.tilePosToScreenPos(posTileMap[0]);
+    }
+    players.push_back(std::make_unique<Player>(gameData, bHandler, false, spawnLoc));
+
+>>>>>>> server
     background.setTexture(gameData->assetManager.getTexture("default background"));
     sf::Vector2f mapSelectorStateBackgroundSize = sf::Vector2f( 
 		static_cast< float >( gameData->assetManager.getTexture("default background").getSize().x ), 
@@ -41,6 +51,10 @@ void InGameState::init(){
         gameData->window.getSize().x/mapSelectorStateBackgroundSize.x, 
         gameData->window.getSize().y/mapSelectorStateBackgroundSize.y
     );
+<<<<<<< 10fa9c9a59fed2b237e9d585c0b62c0cbdde06f1
+=======
+    
+>>>>>>> server
 }
 
 void InGameState::handleInput(){
