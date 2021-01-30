ad410487dcf8666b160274803667b36ae672ded3
BraveKoen <koen@bravemail.nl>
2021-01-30 13:09:44 +0100

    doxygen

--- headers/bomb.hpp
+++ headers/bomb.hpp
@@ -8,27 +8,73 @@
 #include "tile.hpp"
 #include "collision.hpp"
 
+/// \brief
+/// Class Bomb
+/// \details
+/// Bomb is one bomb on the screen with is own time
+
 class Bomb{
 public:
+    /// \brief
+    /// Bomb constructor 
+    /// \details
+    /// Bomb will need gamedata to draw on the screen
+    /// int playerId to keep track on who placed the bomb
+    /// int lenghtx and int lenghtY for how big the explosion is
+    /// float explodeTime for how long it takes to explode
+    /// float timeCreated when to bomb is placed by de player
+    /// sf::Vector2f position where to place the bomb on the map 
     Bomb(gameDataRef data, int playerId, int lenghtX, int lenghtY, float explodeTime, float timeCreated, sf::Vector2f pos);
     ~Bomb(){}
 
+    /// \brief
+    /// void draw
+    /// \details
+    /// draws the sprite on the screen
     void draw();
 
+    /// \brief
+    /// void explode
+    /// \details
+    /// will draw the explosion, the explosion is based on lenghtX and lenghtY
     void explode();
 
+    /// \brief
+    /// bool isExploded
+    /// \details
+    /// isExploded keeps track of the time
+    /// it return true when the time is over
     bool isExploded(float currentTime);
 
+    /// \brief
+    /// int getBombId
+    /// \details
+    /// returns the bombId
     int getBombId();
 
+    /// \brief
+    /// bool playerHit
+    /// \details
+    /// playerHit will return true when the bomb explodes and hits a player
+    /// it will not return true when you hit a opponent
     bool playerHit();
 
+    /// \brief
+    /// void setPos(sf::Vector2F)
+    /// \details
+    /// you can change the position of the bomb when it is placed
     void setPos(sf::Vector2f pos);
 
+    /// \brief
+    /// void clearBomb
+    /// \details
+    /// clearBomb get rid of al the explotion animations
     void clearBomb();
 
     bool bombColliding(const sf::Sprite& target);
+
     void animateFuse();
+    
     void animateExplosion();
 
     bool getPrimed();
--- headers/server.hpp
+++ headers/server.hpp
@@ -27,30 +27,78 @@ private:
     std::vector<std::vector<std::string>> mapDecoder(std::string str);
 
 public:
+    /// \brief
+    /// Server ip for connection with port
+    /// \details
+    ///
     Server(sf::IpAddress ip, unsigned short port);
 
-    void testConnection(int playerNumber, sf::IpAddress ip, unsigned short port);
-    
-    void run();
-
+    /// \brief
+    /// void sendData playerInfo 
+    /// \details
+    /// sends a struct to the server
     void sendData(PlayerInfo &playerInfo);
 
+    /// \brief
+    /// void send lobbyInfo
+    /// \details
+    /// send LobbyData to the server
     void sendData(LobbyInfo &lobbyInfo);
 
+    /// \brief
+    /// void serverGetPLayerId
+    /// \details
+    /// sends a request that you need a playerId
     void serverGetPlayerId(int playerId);
 
+    /// \brief
+    /// void PLayerDisconnect
+    /// \details
+    /// when a player leaves the program or the game this will send a message to te server that the player is leaves
     void playerDisconnect();
+
+    /// \brief
+    /// playerInfo receiveDataInGame
+    /// \details
+    /// will return a playerInfo if it receive a data packets 
+    /// When it receives nothing is will wait!
     PlayerInfo receiveDataInGame();
+
+    /// \brief
+    /// bool receiveDataLobby stopifhost
+    /// \details
+    /// return true when the games starts or when you receive playerId 1
+    /// when the bool is false it will not return true when playerId 1 receives
     bool receiveDataLobby(bool stopIfHost=true);
 
+     /// \brief
+    /// int getPlayerId
+    /// \details
+    /// return playerId from server
     int getPlayerId();
 
+    /// \brief
+    /// void setPlayerId int newPLayerId
+    /// \details
+    /// with this function you can reset the playerId
     void setPlayerId(int newPlayerId);
 
+    /// \brief
+    /// void getOpponentCount
+    /// \details
+    /// returns a int with how many opponents you play against
     int getOpponentCount();
 
+    /// \brief
+    /// void playerReady
+    /// \details
+    /// sends a message to the server when a user says he is ready
     void playerReady();
 
+    /// \brief
+    ///  void hostReady  std::vector<std::vector<std::string>>
+    /// \details
+    /// sends the hole map through and will start the game
     void hostReady(std::vector<std::vector<std::string>> map);
 
     std::vector<std::vector<std::string>> getMap(){return map;}
