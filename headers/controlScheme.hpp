22adbbee60fdea0722962da1afc73b6b0253b0e4
Rick Horeman <rickhoreman@gmail.com>
2021-01-31 13:58:34 +0100

    Doxygen for controlScheme.hpp, game.hpp, inputManager.hpp, modeSelectState.hpp, state.hpp, stateMachine.hpp, tile.hpp and tileMap.hpp

--- headers/controlScheme.hpp
+++ headers/controlScheme.hpp
@@ -3,13 +3,31 @@
 
 #include <SFML/Graphics.hpp>
 
+/// \brief
+/// Simple class for creating basic control schemes.
+/// \details
+/// This class is mainly meant for having multiple control schemes on the same client, i.e. local multiplayer wih people using WASD, IJKL and arrow keys for example.
+/// Currently the class is made specifically for bomberman and only support movement keys and a bomb key, but it can very easily be expanded/changed to, for example, include an 'interaction' key.
 class ControlScheme{
 private:
     sf::Keyboard::Key upKey, leftKey, downKey, rightKey, bombKey;
 public:
+    /// \brief
+    /// ControlScheme constructor
+    /// \details
+    /// ControlSCheme construction, takes sf::Keyboard::key for every input (movement directions + bomb placement).
     ControlScheme(sf::Keyboard::Key upKey, sf::Keyboard::Key leftKey, sf::Keyboard::Key downKey, sf::Keyboard::Key rightKey, sf::Keyboard::Key bombKey); //Keys ordered in standard WASD format, with the extra bomb key at the end
 
+    /// \brief
+    /// Returns the movement direction
+    /// \details
+    /// Returns the movement direction based on which of the directional input keys are pressed. Returns the direction of the movement as a vector with amplitude 1.
     sf::Vector2i getDirection() const;
+
+    /// \brief
+    /// Returns wether the bomb key is pressed.
+    /// \details
+    /// Returns wether the given (in the constructor) bomb key is pressed.
     bool getBombKeyPressed() const;
 };
 
--- headers/tile.hpp
+++ headers/tile.hpp
@@ -4,23 +4,86 @@
 #include <SFML/Graphics.hpp>
 #include <string>
 
+/// \brief
+/// Tile class for TileMap
+/// \details
+/// This class is essentially a basic datatype for the tiles in a TileMap, it contains it's own sprite and a string for it's type.
+/// The class has various getters and setters for freely and easily changing and getting the type and various aspects of the sprite.
 class Tile{
 private:
     std::string type;
     sf::Sprite sprite;
 public:
+    /// \brief
+    /// Tile constructor
+    /// \details
+    /// Can be constructed completely empty, leaving it the basic "empty" type and a default sprite with !no texture! (<- avoid this). Otherwise a type can be specified, possibly along with a sprite. These attributes can also be set through various setters.
     Tile(std::string type="empty", sf::Sprite sprite=sf::Sprite());
 
+    /// \brief
+    /// Setter for tile type
+    /// \details
+    /// Allows you to set the type of the tile to a string newType.
     void setType(std::string newType);
+
+    /// \brief
+    /// Getter for tile type
+    /// \details
+    /// Returns the type of the tile.
     const std::string& getType() const;
+
+    /// \brief
+    /// Setter for tile texture
+    /// \details
+    /// Allows you to set the texture of the tile's sprite.
     void setTexture(const sf::Texture & newTexture);
+
+    /// \brief
+    /// Getter for tile texture
+    /// \details
+    /// Returns a const pointer to the tile's texture.
     const sf::Texture* getTexture() const;
+
+    /// \brief
+    /// Setter for tile position
+    /// \details
+    /// Allows you to set the tile's (sprite's) position.
     void setPosition(const sf::Vector2f & newPosition);
+
+    /// \brief
+    /// Getter for tile position
+    /// \details
+    /// Returns the tile position.
     const sf::Vector2f& getPosition() const;
+
+    /// \brief
+    /// Setter for tile scale
+    /// \details
+    /// Allows you to set the tile's (sprite's) scale.
     void setScale(const sf::Vector2f & newScale);
+
+    /// \brief
+    /// Getter for tile scale
+    /// \details
+    /// Returns the tile's (sprite's) scale.
     const sf::Vector2f& getScale() const;
+
+    /// \brief
+    /// Getter for tile size
+    /// \details
+    /// Returns the tile's (sprite's) size.
     const sf::Vector2f getSize() const;
+
+    /// \brief
+    /// Getter for tile bounds
+    /// \details
+    /// Return the tile's (sprite's) global bounds.
     const sf::FloatRect getBounds() const;
+
+    /// \brief
+    /// Draws the tile
+    /// \details
+    /// Draws the tile on the given window.
     void draw(sf::RenderWindow & window) const;
 };
 
