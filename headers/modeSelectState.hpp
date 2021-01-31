6101ff6cb3779b3179460e5f4d3fad5ce48c01c6
Rick Horeman <rickhoreman@gmail.com>
2021-01-31 13:58:34 +0100

    Doxygen for controlScheme.hpp, game.hpp, inputManager.hpp, modeSelectState.hpp, state.hpp, stateMachine.hpp, tile.hpp and tileMap.hpp

--- headers/modeSelectState.hpp
+++ headers/modeSelectState.hpp
@@ -14,6 +14,11 @@
 #include "mapSelectorState.hpp"
 #include "tileMap.hpp"
 
+/// \brief
+/// State for selecting game mode
+/// \details
+/// In this state the user can choose for local or online multiplayer, if local also the number of players.
+/// Some server connection stuff is also done here, it acts as a sort of lobby for online.
 class ModeSelectState: public State {
 private:
     gameDataRef gameData;
@@ -29,14 +34,46 @@ private:
     bool startMapSelect = false;
     bool oneTimePress = true;
 public:
+    /// \brief
+    /// Constructor for ModeSelectState
+    /// \details
+    /// Only takes a gameDataRef, this constructor doesnt really do much besided the initialization of a couple variables.
     ModeSelectState(gameDataRef gameData);
+
+    /// \brief
+    /// Init function for ModeSelectState, overrides State::init
+    /// \details
+    /// This function initiates most of the screen objects this state has, this function is called first by the StateMachine, after it come the handleInput, update and draw functions.
     virtual void init() override;
+
+    /// \brief
+    /// handleInput function for ModeSelectState, overrides State::handleInput
+    /// \details
+    /// This function handles the user input for the entire state, such as the clicking on buttons, or the closing of the window.
     virtual void handleInput() override;
+
+    /// \brief
+    /// update function for ModeSelectState, overrides State::update
+    /// \details
+    /// This function updates everything that isnt user input for the state, in this state this boils down to mostly online multiplayer related stuff.
     virtual void update(float delta) override;
+
+    /// \brief
+    /// draw function for ModeSelectState, overrides State::draw
+    /// \details
+    /// This function draws all screen objects for the class, in some cases dependant on state variables, i.e. showing the player number buttons after the 'local' button has been pressed.
     virtual void draw(float delta) override;
 
+    /// \brief
+    /// Makes buttons from sets of buttondata
+    /// \details
+    /// This function instantiates most of the button used in this state from a vector of buttonDataExt. It returns a vector of MenuButtonExt that were placed based on the number of buttonDataExt and the offset.
     std::vector<MenuButtonExt> makeButtons(std::vector<buttonDataExt> buttonData, sf::Vector2f offset={0,0});
 
+    /// \brief
+    /// Handles server connection for online
+    /// \details
+    /// This function is usually only called in a seperate thread and is used for lobby related commucation between the client and server.
     void lobbyQueue();
 };
 
--- headers/stateMachine.hpp
+++ headers/stateMachine.hpp
@@ -8,6 +8,13 @@
 
 using stateRef = std::unique_ptr<State>;
 
+/// \brief
+/// StateMachine class, handles game states
+/// \details
+/// This class manages the different states of the game, to accomplish this it has a stack of states that can be added to (with or without replacing) and removed from.
+/// Whenever a state is added, without replacing the previous, the StateMachine calls the pause function of the current state before doing so.'
+/// Whenever a state is removed the resume function of the new current state is called afterwards.
+/// The StateMachine has a seperate processStateChanges function that is called by the game loop after finishing everything for the current state, this means that when a state calls for a statechange it's current loop is finished first.
 class StateMachine{
 private:
     std::stack<stateRef> statesStack;
@@ -16,10 +23,31 @@ private:
     bool isAdding;
     bool isReplacing;
 public:
+    /// \brief
+    /// Function for adding a new state to the stack
+    /// \details
+    /// This function essentially tells the StateMachine that it wants to add a new state, and which one, but doesnt actually do it yet. 
+    /// Instead a boolean is set and the new state is saved for the time being to be processed when the processStateChanges function is next called.
     void addState(stateRef state, bool replacing=true);
+
+    /// \brief
+    /// Function for removing the current state from the stack
+    /// \details
+    /// This function essentially tells the StateMachine that it wants to get rid of the current state, and resume the one under it.
+    /// Instead of immediately doing this it sets a boolean to be processed in the next processStateChanges call.
     void removeState();
+
+    /// \brief
+    /// Function that processes all new state changes
+    /// \details
+    /// This function check if the state changing booleans are set and if so handles their requests by adding a new state, with or without replacing, or removing the current state.
+    /// This function is also the one that calls the pause and resume functions when appropriate.
     void processStateChanges();
 
+    /// \brief
+    /// Getter for the active state
+    /// \details
+    /// This function returns a reference to the current active state. This is mostly used by the game loop to call the handleInput, update and draw functions of the currently active state.
     stateRef & getActiveState();
 };
 
