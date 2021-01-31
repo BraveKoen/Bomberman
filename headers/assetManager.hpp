6ff45369b29df9191370788de54f02c957213871
joeri-hu <joeri.j.kok@student.hu.nl>
2021-01-31 04:20:43 +0100

    improved texture loading mechanism

--- headers/assetManager.hpp
+++ headers/assetManager.hpp
@@ -17,6 +17,11 @@ public:
     const sf::Font& loadFont(const resourceData& resource);
     const sf::Font& loadFont(std::string name, std::string fileName);
 
+    const sf::Texture& reloadTexture(const resourceData& resource);
+    const sf::Texture& reloadTexture(std::string name, std::string fileName);
+    const sf::Font& reloadFont(const resourceData& resource);
+    const sf::Font& reloadFont(std::string name, std::string fileName);
+
     const sf::Texture& getTexture(const resourceData& resource) const;
     const sf::Texture& getTexture(std::string name) const;
     const sf::Font& getFont(const resourceData& resource) const;
